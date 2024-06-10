d098a2068d0310c1a57f3fbab046aa3c2a74c854
Sulman Khan <sulman@vt.edu>
2024-06-10 15:25:38 +0000

    fixed facts_trip

--- 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fact_trips.sql
@@ -21,7 +21,7 @@ trips_unioned as (
     select * from yellow_tripdata
 ),
 dim_zones as (
-    select * from {{ ref('taxi_zone_lookup') }}
+    select * from {{ ref('dim_zones') }}
     where borough != 'Unknown'
 )
 
